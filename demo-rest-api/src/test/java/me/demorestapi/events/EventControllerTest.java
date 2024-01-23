bc4b87aeb93827bf853064b519911091ca659aea
suhyungk <dook4966@gmail.com>
2024-01-23 15:52:07 +0900

    [REST API] Event Repository + polishing

--- demo-rest-api/src/test/java/me/demorestapi/events/EventControllerTest.java
+++ demo-rest-api/src/test/java/me/demorestapi/events/EventControllerTest.java
@@ -3,9 +3,12 @@ package me.demorestapi.events;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,8 +18,7 @@ import java.time.LocalDateTime;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -25,6 +27,8 @@ public class EventControllerTest {
     MockMvc mockMvc;
     @Autowired
     ObjectMapper objectMapper;
+    @MockBean
+    EventRepository eventRepository;
 
     @Test
     public void createEvent() throws Exception {
@@ -40,6 +44,9 @@ public class EventControllerTest {
                            .limitOfEnrollment(100)
                            .location("강남역 D2 스타트업 팩토리")
                            .build();
+        event.setId(10);
+        Mockito.when(eventRepository.save(event))
+               .thenReturn(event);
 
         // perform 안에 요청을 주면 응답이 나옴
         mockMvc.perform(post("/api/events/")
@@ -49,6 +56,8 @@ public class EventControllerTest {
                )
                .andDo(print())
                .andExpect(status().isCreated())
-               .andExpect(jsonPath("id").exists());
+               .andExpect(jsonPath("id").exists())
+               .andExpect(header().exists(HttpHeaders.LOCATION))
+               .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
     }
 }
