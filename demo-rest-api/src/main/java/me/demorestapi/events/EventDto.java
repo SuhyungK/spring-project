07487896d7ec389c65179e10a7ae1153a7c2c482
suhyungk <dook4966@gmail.com>
2024-01-23 16:26:57 +0900

    [REST API] EventDto 를 사용한 입력값 제한 하기

--- demo-rest-api/src/main/java/me/demorestapi/events/EventDto.java
+++ demo-rest-api/src/main/java/me/demorestapi/events/EventDto.java
@@ -1,16 +1,15 @@
 package me.demorestapi.events;
 
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Builder @AllArgsConstructor @NoArgsConstructor
-@Getter @Setter @EqualsAndHashCode(of = "id")
-@Entity
-public class Event {
-    @Id @GeneratedValue
-    private Integer id;
+@Builder @NoArgsConstructor @AllArgsConstructor
+@Data
+public class EventDto {
     private String name;
     private String description;
     private LocalDateTime beginEnrollmentDateTime;
@@ -21,8 +20,4 @@ public class Event {
     private int basePrice;
     private int maxPrice;
     private int limitOfEnrollment;
-    private boolean offline;
-    private boolean free;
-    @Enumerated(EnumType.STRING)
-    private EventStatus eventStatus = EventStatus.DRAFT;
 }
--- demo-rest-api/src/test/java/me/demorestapi/events/EventControllerTest.java
+++ demo-rest-api/src/test/java/me/demorestapi/events/EventControllerTest.java
@@ -1,11 +1,13 @@
 package me.demorestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
@@ -15,24 +17,23 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTest {
     @Autowired
     MockMvc mockMvc;
     @Autowired
     ObjectMapper objectMapper;
-    @MockBean
-    EventRepository eventRepository;
 
     @Test
     public void createEvent() throws Exception {
         Event event = Event.builder()
+                           .id(100)
                            .name("Spring")
                            .description("REST API Development with Spring")
                            .beginEnrollmentDateTime(LocalDateTime.of(2024, 1, 23, 14, 21))
@@ -43,10 +44,10 @@ public class EventControllerTest {
                            .maxPrice(200)
                            .limitOfEnrollment(100)
                            .location("강남역 D2 스타트업 팩토리")
+                           .free(true)
+                           .offline(false)
+                           .eventStatus(EventStatus.PUBLISHED)
                            .build();
-        event.setId(10);
-        Mockito.when(eventRepository.save(event))
-               .thenReturn(event);
 
         // perform 안에 요청을 주면 응답이 나옴
         mockMvc.perform(post("/api/events/")
@@ -58,6 +59,10 @@ public class EventControllerTest {
                .andExpect(status().isCreated())
                .andExpect(jsonPath("id").exists())
                .andExpect(header().exists(HttpHeaders.LOCATION))
-               .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
+               .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+               .andExpect(jsonPath("id").value(Matchers.not(100)))
+               .andExpect(jsonPath("free").value(Matchers.not(true)))
+               .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+        ;
     }
 }
