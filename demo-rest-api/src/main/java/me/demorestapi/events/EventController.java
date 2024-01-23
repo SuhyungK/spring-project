7e03e90fbd2248e16a323ef700b673b875f9621e
suhyungk <dook4966@gmail.com>
2024-01-23 15:42:29 +0900

    [REST API] Event Controller

--- demo-rest-api/src/main/java/me/demorestapi/events/EventController.java
+++ demo-rest-api/src/main/java/me/demorestapi/events/EventController.java
@@ -1,4 +1,26 @@
 package me.demorestapi.events;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@Slf4j
+@Controller
+@RequestMapping(value = "/api/events/", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event) {
+        URI createdUri = linkTo(EventController.class).slash("{id}")
+                                                      .toUri();
+        event.setId(10);
+        return ResponseEntity.created(createdUri)
+                             .body(event);
+    }
 }
--- demo-rest-api/src/test/java/me/demorestapi/events/EventControllerTest.java
+++ demo-rest-api/src/test/java/me/demorestapi/events/EventControllerTest.java
@@ -1,5 +1,6 @@
 package me.demorestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -18,13 +23,32 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class EventControllerTest {
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
-    void createEvent() throws Exception {
+    public void createEvent() throws Exception {
+        Event event = Event.builder()
+                           .name("Spring")
+                           .description("REST API Development with Spring")
+                           .beginEnrollmentDateTime(LocalDateTime.of(2024, 1, 23, 14, 21))
+                           .closeEnrollmentDateTime(LocalDateTime.of(2024, 1, 24, 14, 21))
+                           .beginEventDateTime(LocalDateTime.of(2024, 1, 25, 14, 16))
+                           .endEventDateTime(LocalDateTime.of(2024, 1, 26, 14, 16))
+                           .basePrice(100)
+                           .maxPrice(200)
+                           .limitOfEnrollment(100)
+                           .location("강남역 D2 스타트업 팩토리")
+                           .build();
+
         // perform 안에 요청을 주면 응답이 나옴
         mockMvc.perform(post("/api/events/")
-                       .contentType(MediaType.APPLICATION_JSON_UTF8)
-                       .accept(MediaTypes.HAL_JSON))
-               .andExpect(status().isCreated());
+                       .contentType(MediaType.APPLICATION_JSON_VALUE)
+                       .accept(MediaTypes.HAL_JSON)
+                       .content(objectMapper.writeValueAsString(event))
+               )
+               .andDo(print())
+               .andExpect(status().isCreated())
+               .andExpect(jsonPath("id").exists());
     }
 }
