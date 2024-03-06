af94fe545379603d8c4f812d5d79c828f8e7cf83
suhyungk <dook4966@gmail.com>
2024-03-06 22:29:53 +0900

    [simollu] init commit

---
+++ simollu/src/test/java/pjt/simollu/SimolluApplicationTests.java
@@ -0,0 +1,13 @@
+package pjt.simollu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimolluApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
